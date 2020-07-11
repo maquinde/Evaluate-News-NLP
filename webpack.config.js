ae551ecc859f3861163e3e5268b82df6e07cc6fb
Mike <maquinde@outlook.com>
2020-07-11 13:57:20 -0700

    1. added html-webpack-plugin 2. configured the server to point to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/client/index.js',
 
     module: {
-        rules: [
-                {
+        rules: [{
             test: '/\.js$/',
             exclude: /node_modules/,
             loader: "babel-loader"
-                }
-        ]
-    } //END MODULE
+        }]
+    }, //END MODULE
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 
 }
